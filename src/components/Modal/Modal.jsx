0d50db776dd3858e46981068f4ef78c69f9de52b
Ulysse09 <upkabalisa@gmail.com>
2024-03-11 11:30:01 +0200

    20th com

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Modal = ({ handletoggle }) => {
   return (
     <>
-      <div className="absolute font-roboto lg:hidden z-50 items-start pl-4 pt-4  inset-5 top-[4rem] bottom-[20rem]      flex flex-col bg-slate-100 bg-gradient-to-b from-white   text-blue-400 space-y-6">
+      <div className="absolute font-roboto lg:hidden z-50 items-start pl-4 pt-4  inset-5 top-[8rem] bottom-[20rem]      flex flex-col bg-slate-100 bg-gradient-to-b from-white   text-blue-400 space-y-6">
         <Link to="about" onClick={handletoggle}>
           About Us
         </Link>
